fd340463de13b1ec6a1b3af020449c8851794c04
Moritz Roessler <morrossl@publicisgroupe.net>
2023-08-10 19:44:19 +0200

    feat: serialize store

--- dist/instances.js
+++ dist/instances.js
@@ -1,14 +1,18 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.store = exports.pubsub = void 0;
 var _graphqlSubscriptions = require("graphql-subscriptions");
 var _reactServer = require("@state-less/react-server");
+var _logger = _interopRequireDefault(require("./lib/logger"));
 var pubsub = new _graphqlSubscriptions.PubSub();
 exports.pubsub = pubsub;
 var store = new _reactServer.Store({
-  scope: 'root'
+  file: './store.json',
+  logger: _logger["default"]
 });
 exports.store = store;
+store.sync(20 * 1000);
--- src/instances.ts
+++ src/instances.ts
@@ -1,5 +1,8 @@
 import { PubSub } from 'graphql-subscriptions';
 import { Store } from '@state-less/react-server';
+import logger from './lib/logger';
 
 export const pubsub = new PubSub();
-export const store = new Store({ scope: 'root' });
+export const store = new Store({ file: './store.json', logger });
+
+store.sync(20 * 1000);
